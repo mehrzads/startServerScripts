9cc3db176f833c897017d9f90288cd5ac2c33fee
mehrzads <mehrzadsamadi@gmail.com>
2015-01-16 11:56:22 -0500

    get the results

--- launch.sh
+++ launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 starcluster start largecluster
 starcluster sshmaster largecluster 'bash -s' < local_script.sh
+starcluster get largecluster /home/sgeadmin/gemm/scalapack/result.txt .
+
 # $Id: $
 
 
